5b3dd9e6e5afdafbe12717db69d6b3a8a32e0073
JoaquimAlt <joaquimferreira1818@gmail.com>
2022-05-08 14:25:35 -0300

    navegacao js, adaptacao desktop

--- main.js
+++ main.js
@@ -3,6 +3,37 @@ onScroll()
 function onScroll(){
     showNavOnScroll()
     backToTopButtonOnScroll()
+    activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // verificar se a base está abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
@@ -36,7 +67,7 @@ ScrollReveal({
 }).reveal(`#home,
          #home img,
          #home .caixa_grande,
-         #home button,
+         #home .button,
          #services,
          #services header,
          #services .card,
@@ -48,4 +79,4 @@ ScrollReveal({
          #contact img
          #contact header,
          #contact ul,
-         #contact button`);
+         #contact .button`);
